db80952f1734ffe9e4b8227d4b958cfdba9a77ef
Ikersito63 <ikerpecas2012@gmail.com>
2025-05-31 11:30:52 -0600

    Create M1L1.py

---
+++ M1L1.py
@@ -0,0 +1,8 @@
+meme_dict = {
+    "cringe": "Algo raro o embarazoso",
+    "lol" : " una respuesta comun a algo grasioso",
+    "rofl" : "es una reaccion a elgo grasioso como lol",
+    "creepy" : "elgo aterrador o ciniestro",
+    "pive" : "persono desconocida o en algunos casos conocida",
+}
+word = input("Escribe una palabra que no entiendas :")
