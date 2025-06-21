59916deaadc39777cb2b0d1197b42d8fb200f666
Ikersito63 <ikerpecas2012@gmail.com>
2025-06-21 17:17:11 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,12 @@ import discord
 import random
 
 
-# La variable intents almacena los privilegios del bot
-intents = discord.Intents.default()
-# Activar el privilegio de lectura de mensajes
+intents = discord.Intents.default()s
 intents.message_content = True
-# Crear un bot en la variable cliente y transferirle los privilegios
+
 client = discord.Client(intents=intents)
 
-@client.event
+@client.event 
 async def on_ready():
     print(f'Hemos iniciado sesión como {client.user}')
 
@@ -17,10 +15,10 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('$hello'):
-        await message.channel.send("Hi!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\U0001f642")
+    if message.content.startswith('hello'):
+        await message.channel.send("Hi")
+    elif message.content.startswith('bye'):
+        await message.channel.send("u5g66f784")
     else:
         await message.channel.send(message.content)
 @client.event 
